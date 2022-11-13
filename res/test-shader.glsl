fb320fc29a803ad4154a80c09d798734cc8f5b7f
Evan <evanlavender13@gmail.com>
2022-11-12 23:03:13 -0500

    Dynamic texture example

--- res/test-shader.glsl
+++ res/test-shader.glsl
@@ -1,28 +1,32 @@
 @ctype mat4 hmm_mat4
 
 @vs vs
-uniform vs_params 
-{
+uniform vs_params {
     mat4 mvp;
 };
 
 in vec4 position;
-out vec3 uvw;
+in vec4 color0;
+in vec2 texcoord0;
+
+layout(location=1) out vec2 uv;
+layout(location=0) out vec4 color;
 
 void main() {
     gl_Position = mvp * position;
-    uvw = position.xyz;
+    uv = texcoord0;
+    color = color0;
 }
 @end
 
 @fs fs
-uniform sampler3D tex;
-
-in vec3 uvw;
+uniform sampler2D tex;
+layout(location=0) in vec4 color;
+layout(location=1) in vec2 uv;
 out vec4 frag_color;
 
 void main() {
-    frag_color = texture(tex, uvw);
+    frag_color = texture(tex, uv) * color;
 }
 @end
 
--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include "math.h"
+#include "stdio.h"
+#include "stdlib.h"
 
 #include "sokol_app.h"
 #include "sokol_gfx.h"
@@ -8,10 +10,14 @@
 #define HANDMADE_MATH_NO_SSE
 #include "HandmadeMath.h"
 #include "cdbgui/cdbgui.h"
+#include "stb/stb_perlin.h"
 
 #include "../res/test-shader.glsl.h"
 
-#define TEX3D_DIM (32)
+#define IMAGE_WIDTH  (64)
+#define IMAGE_HEIGHT (64)
+#define LIVING (0xFFFFFFFF)
+#define DEAD (0xFF000000)
 
 static struct 
 {
@@ -19,15 +25,13 @@ static struct
     sg_pipeline pipeline;
     sg_bindings bindings;
     float rx, ry, t;
+
+    int update_count;
+    uint32_t pixels[IMAGE_WIDTH][IMAGE_HEIGHT];
 } state;
 
-static uint32_t xorshift32() {
-    static uint32_t x = 0x12345678;
-    x ^= x<<13;
-    x ^= x>>17;
-    x ^= x<<5;
-    return x;
-}
+static void game_of_life_init();
+static void game_of_life_update();
 
 void init() 
 {
@@ -53,12 +57,36 @@ void init()
 
     float vertices[] = 
     {
-        -1.0, -1.0, -1.0,    1.0, -1.0, -1.0,    1.0,  1.0, -1.0,   -1.0,  1.0, -1.0,
-        -1.0, -1.0,  1.0,    1.0, -1.0,  1.0,    1.0,  1.0,  1.0,   -1.0,  1.0,  1.0,
-        -1.0, -1.0, -1.0,   -1.0,  1.0, -1.0,   -1.0,  1.0,  1.0,   -1.0, -1.0,  1.0,
-         1.0, -1.0, -1.0,    1.0,  1.0, -1.0,    1.0,  1.0,  1.0,    1.0, -1.0,  1.0,
-        -1.0, -1.0, -1.0,   -1.0, -1.0,  1.0,    1.0, -1.0,  1.0,    1.0, -1.0, -1.0,
-        -1.0,  1.0, -1.0,   -1.0,  1.0,  1.0,    1.0,  1.0,  1.0,    1.0,  1.0, -1.0,
+        /* pos                  color                       uvs */
+        -1.0f, -1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f,     0.0f, 0.0f,
+        1.0f, -1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f,     1.0f, 0.0f,
+        1.0f,  1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f,     1.0f, 1.0f,
+        -1.0f,  1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f,     0.0f, 1.0f,
+
+        -1.0f, -1.0f,  1.0f,    0.0f, 1.0f, 0.0f, 1.0f,     0.0f, 0.0f,
+        1.0f, -1.0f,  1.0f,    0.0f, 1.0f, 0.0f, 1.0f,     1.0f, 0.0f,
+        1.0f,  1.0f,  1.0f,    0.0f, 1.0f, 0.0f, 1.0f,     1.0f, 1.0f,
+        -1.0f,  1.0f,  1.0f,    0.0f, 1.0f, 0.0f, 1.0f,     0.0f, 1.0f,
+
+        -1.0f, -1.0f, -1.0f,    0.0f, 0.0f, 1.0f, 1.0f,     0.0f, 0.0f,
+        -1.0f,  1.0f, -1.0f,    0.0f, 0.0f, 1.0f, 1.0f,     1.0f, 0.0f,
+        -1.0f,  1.0f,  1.0f,    0.0f, 0.0f, 1.0f, 1.0f,     1.0f, 1.0f,
+        -1.0f, -1.0f,  1.0f,    0.0f, 0.0f, 1.0f, 1.0f,     0.0f, 1.0f,
+
+        1.0f, -1.0f, -1.0f,    1.0f, 0.5f, 0.0f, 1.0f,     0.0f, 0.0f,
+        1.0f,  1.0f, -1.0f,    1.0f, 0.5f, 0.0f, 1.0f,     1.0f, 0.0f,
+        1.0f,  1.0f,  1.0f,    1.0f, 0.5f, 0.0f, 1.0f,     1.0f, 1.0f,
+        1.0f, -1.0f,  1.0f,    1.0f, 0.5f, 0.0f, 1.0f,     0.0f, 1.0f,
+
+        -1.0f, -1.0f, -1.0f,    0.0f, 0.5f, 1.0f, 1.0f,     0.0f, 0.0f,
+        -1.0f, -1.0f,  1.0f,    0.0f, 0.5f, 1.0f, 1.0f,     1.0f, 0.0f,
+        1.0f, -1.0f,  1.0f,    0.0f, 0.5f, 1.0f, 1.0f,     1.0f, 1.0f,
+        1.0f, -1.0f, -1.0f,    0.0f, 0.5f, 1.0f, 1.0f,     0.0f, 1.0f,
+
+        -1.0f,  1.0f, -1.0f,    1.0f, 0.0f, 0.5f, 1.0f,     0.0f, 0.0f,
+        -1.0f,  1.0f,  1.0f,    1.0f, 0.0f, 0.5f, 1.0f,     1.0f, 0.0f,
+        1.0f,  1.0f,  1.0f,    1.0f, 0.0f, 0.5f, 1.0f,     1.0f, 1.0f,
+        1.0f,  1.0f, -1.0f,    1.0f, 0.0f, 0.5f, 1.0f,     0.0f, 1.0f
     };
 
     state.bindings.vertex_buffers[0] = sg_make_buffer(&(sg_buffer_desc)
@@ -69,13 +97,14 @@ void init()
     });
 
     uint16_t indices[] = { 
-        0, 1, 2,     0, 2, 3,
-        6, 5, 4,     7, 6, 4,
-        8, 9, 10,    8, 10, 11,
+        0, 1, 2,  0, 2, 3,
+        6, 5, 4,  7, 6, 4,
+        8, 9, 10,  8, 10, 11,
         14, 13, 12,  15, 14, 12,
         16, 17, 18,  16, 18, 19,
-        22, 21, 20,  23, 22, 20        
+        22, 21, 20,  23, 22, 20   
     };
+
     state.bindings.index_buffer = sg_make_buffer(&(sg_buffer_desc)
     {
         .type = SG_BUFFERTYPE_INDEXBUFFER,
@@ -85,49 +114,53 @@ void init()
 
     state.pipeline = sg_make_pipeline(&(sg_pipeline_desc) 
     {
+        .layout = {
+            .attrs = {
+                [ATTR_vs_position].format  = SG_VERTEXFORMAT_FLOAT3,
+                [ATTR_vs_color0].format    = SG_VERTEXFORMAT_FLOAT4,
+                [ATTR_vs_texcoord0].format = SG_VERTEXFORMAT_FLOAT2
+            }
+        },
         .shader = shader,
         .index_type = SG_INDEXTYPE_UINT16,
         .cull_mode = SG_CULLMODE_BACK,
-        .depth = 
-        {
+        .depth = {
             .compare = SG_COMPAREFUNC_LESS_EQUAL,
-            .write_enabled = true,
-        },
-        .layout = 
-        {
-            .attrs = 
-            {
-                [ATTR_vs_position].format = SG_VERTEXFORMAT_FLOAT3
-            }
+            .write_enabled = true
         },
         .label = "cube-pipeline"
     });
 
-    static uint32_t pixels[TEX3D_DIM][TEX3D_DIM][TEX3D_DIM];
-    for (int x = 0; x < TEX3D_DIM; x++)
-    {
-        for (int y = 0; y < TEX3D_DIM; y++)
-        {
-            for (int z = 0; z < TEX3D_DIM; z++)
-            {
-                pixels[x][y][z] = xorshift32();
-            }
-        }
-    }
+    // for (int x = 0; x < IMAGE_WIDTH; x++)
+    // {
+    //     for (int y = 0; y < IMAGE_HEIGHT; y++)
+    //     {
+    //         // pixels[x][y][z] = xorshift32();
+    //         float val = stb_perlin_ridge_noise3(
+    //             (float) x / IMAGE_WIDTH,
+    //             (float) y / IMAGE_HEIGHT,
+    //             (float) x / IMAGE_WIDTH,
+    //             2.0f, 0.5f, 1.0f, 8);
+    //         // printf("x,y,z=[%f %f %f] val=%f\n", (float) x, (float) y, (float) z, val);
+    //         state.pixels[x][y] = 255;
+    //         printf("pixels[x, y, z]=%i val=%f\n", state.pixels[x][y], val);
+    //     }
+    // }
 
     state.bindings.fs_images[SLOT_tex] = sg_make_image(&(sg_image_desc)
     {
-        .type = SG_IMAGETYPE_3D,
-        .width = TEX3D_DIM,
-        .height = TEX3D_DIM,
-        .num_slices = TEX3D_DIM,
-        .num_mipmaps = 1,
+        .width = IMAGE_WIDTH,
+        .height = IMAGE_HEIGHT,
         .pixel_format = SG_PIXELFORMAT_RGBA8,
+        .usage = SG_USAGE_STREAM,
         .min_filter = SG_FILTER_LINEAR,
         .mag_filter = SG_FILTER_LINEAR,
-        .label = "random texture",
-        .data.subimage[0][0] = SG_RANGE(pixels)
+        .wrap_u = SG_WRAP_CLAMP_TO_EDGE,
+        .wrap_v = SG_WRAP_CLAMP_TO_EDGE,
+        .label = "dynamic-texture"
     });
+
+    game_of_life_init();
 }
 
 void frame() 
@@ -143,10 +176,18 @@ void frame()
     hmm_mat4 rxm = HMM_Rotate(state.rx, HMM_Vec3(1.0f, 0.0f, 0.0f));
     hmm_mat4 rym = HMM_Rotate(state.ry, HMM_Vec3(0.0f, 1.0f, 0.0f));
     hmm_mat4 model = HMM_MultiplyMat4(rxm, rym);
+
     vs_params_t vs_params = 
     {
         .mvp = HMM_MultiplyMat4(view_proj, model),
     };
+
+    game_of_life_update();
+
+    sg_update_image(state.bindings.fs_images[0], &(sg_image_data)
+    {
+        .subimage[0][0] = SG_RANGE(state.pixels)
+    });
 
     sg_begin_default_pass(&state.pass_action, sapp_width(), sapp_height());
     sg_apply_pipeline(state.pipeline);
@@ -162,6 +203,56 @@ void cleanup()
 {
     __cdbgui_shutdown();
     sg_shutdown();
+}
+
+void game_of_life_init() {
+    for (int y = 0; y < IMAGE_HEIGHT; y++) {
+        for (int x = 0; x < IMAGE_WIDTH; x++) {
+            if ((rand() & 255) > 230) {
+                state.pixels[y][x] = LIVING;
+            }
+            else {
+                state.pixels[y][x] = DEAD;
+            }
+        }
+    }
+}
+
+void game_of_life_update() {
+    for (int y = 0; y < IMAGE_HEIGHT; y++) {
+        for (int x = 0; x < IMAGE_WIDTH; x++) {
+            int num_living_neighbours = 0;
+            for (int ny = -1; ny < 2; ny++) {
+                for (int nx = -1; nx < 2; nx++) {
+                    if ((nx == 0) && (ny == 0)) {
+                        continue;
+                    }
+                    if (state.pixels[(y+ny)&(IMAGE_HEIGHT-1)][(x+nx)&(IMAGE_WIDTH-1)] == LIVING) {
+                        num_living_neighbours++;
+                    }
+                }
+            }
+            /* any live cell... */
+            if (state.pixels[y][x] == LIVING) {
+                if (num_living_neighbours < 2) {
+                    /* ... with fewer than 2 living neighbours dies, as if caused by underpopulation */
+                    state.pixels[y][x] = DEAD;
+                }
+                else if (num_living_neighbours > 3) {
+                    /* ... with more than 3 living neighbours dies, as if caused by overpopulation */
+                    state.pixels[y][x] = DEAD;
+                }
+            }
+            else if (num_living_neighbours == 3) {
+                /* any dead cell with exactly 3 living neighbours becomes a live cell, as if by reproduction */
+                state.pixels[y][x] = LIVING;
+            }
+        }
+    }
+    if (state.update_count++ > 240) {
+        game_of_life_init();
+        state.update_count = 0;
+    }
 }
 
 void event(const sapp_event *e) 
